89534ba266fbce27a38c0eb57643565aede4dd43
StefanoScolari <stefano.scolari@studio.unibo.it>
2023-03-24 20:20:23 +0100

    Changed img resolution

--- src/main.cpp
+++ src/main.cpp
@@ -64,7 +64,7 @@ color ray_color(const ray& r, const hittable& world, int depth){
 int main() {
     //Image
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const int image_width = 1080;
     const int image_height = static_cast<int>(image_width/aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
